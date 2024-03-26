e490f791c5840befbf66685137d6d9a4f4f788fa
jeonguk <leejeongwk98@gmail.com>
2024-03-27 00:03:58 +0900

    feat: default layouts

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,18 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./router";
+import { projectVersion } from "./utils/config";
 
 function App() {
   return (
-    <div className="bg-slate-300">
-      <header>zz</header>
-      <main>
+    <div className="flex flex-col h-screen content-between">
+      <header className="h-8 bg-slate-400">헤더 개발필요..</header>
+      <main className="grow">
         <RouterProvider router={router} />
       </main>
-      <footer>foot</footer>
+      <footer className="text-center text-xs">
+        hyojason-client version: {projectVersion}
+      </footer>
     </div>
   );
 }
