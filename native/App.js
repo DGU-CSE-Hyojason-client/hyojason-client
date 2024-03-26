a1b0152af125b5b076ec4c09a6448d2d75a0db92
jeonguk <leejeongwk98@gmail.com>
2024-03-26 22:32:20 +0900

    feat: configure production

--- native/App.js
+++ native/App.js
@@ -1,10 +1,11 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import WebView from "react-native-webview";
 
 export default function App() {
   return (
     <WebView
       style={styles.container}
+      originWhitelist={["*"]}
       source={{ uri: process.env.EXPO_PUBLIC_WEBVIEW_URL }}
     />
   );
