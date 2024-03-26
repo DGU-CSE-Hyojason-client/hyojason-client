e37a124c8de6d59c636ebb7bcfd86c1143fe236a
jeonguk <leejeongwk98@gmail.com>
2024-03-26 22:48:22 +0900

    feat: add css props to use

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,22 @@
+import { css } from "@emotion/react";
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import { router } from "./router";
 
 function App() {
-  return <div>hyojason web</div>;
+  return (
+    <div
+      css={css`
+        color: black;
+      `}
+    >
+      <header>zz</header>
+      <main>
+        <RouterProvider router={router} />
+      </main>
+      <footer>foot</footer>
+    </div>
+  );
 }
 
 export default App;
