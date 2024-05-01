d893c0285451c0d9f9066dd474a69aa306e0e7c7
jeonguk <leejeongwk98@gmail.com>
2024-05-01 17:20:21 +0900

    fix: build error

--- web/src/_mocks/index.ts
+++ web/src/_mocks/index.ts
@@ -1,8 +1,5 @@
 const initMock = async (): Promise<void> => {
-  if (typeof window === "undefined") {
-    const { server } = await import("./node");
-    server.listen();
-  } else {
+  if (typeof window !== "undefined") {
     const { worker } = await import("./browser");
     await worker.start();
   }
