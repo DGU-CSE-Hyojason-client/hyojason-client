c16005feaa75927b47c83a2c4624c2b19557dd2c
jeonguk <leejeongwk98@gmail.com>
2024-05-01 18:02:29 +0900

    chore: update package.json

--- native/App.js
+++ native/App.js
@@ -99,32 +99,40 @@ export default function App() {
   }, []);
 
   return (
-    <View
-      style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}
-    >
-      <Text>Your expo push token: {expoPushToken}</Text>
-      <View style={{ alignItems: "center", justifyContent: "center" }}>
-        <Text>
-          Title: {notification && notification.request.content.title}{" "}
-        </Text>
-        <Text>Body: {notification && notification.request.content.body}</Text>
-        <Text>
-          Data:{" "}
-          {notification && JSON.stringify(notification.request.content.data)}
-        </Text>
-      </View>
-      <Button
-        title="Press to Send Notification"
-        onPress={async () => {
-          await sendPushNotification(expoPushToken);
-        }}
+    // <View
+    //   style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}
+    // >
+    //   <Text>Your expo push token: {expoPushToken}</Text>
+    //   <View style={{ alignItems: "center", justifyContent: "center" }}>
+    //     <WebView
+    //       style={styles.container}
+    //       originWhitelist={["*"]}
+    //       source={{ uri: process.env.EXPO_PUBLIC_WEBVIEW_URL }}
+    //     />
+    //     <Text>
+    //       Title: {notification && notification.request.content.title}{" "}
+    //     </Text>
+    //     <Text>Body: {notification && notification.request.content.body}</Text>
+    //     <Text>
+    //       Data:{" "}
+    //       {notification && JSON.stringify(notification.request.content.data)}
+    //     </Text>
+    //   </View>
+    //   <Button
+    //     title="Press to Send Notification"
+    //     onPress={async () => {
+    //       await sendPushNotification(expoPushToken);
+    //     }}
+    //   />
+    // </View>
+    <>
+      <View></View>
+      <WebView
+        style={{ width: "300px" }}
+        originWhitelist={["*"]}
+        source={{ uri: process.env.EXPO_PUBLIC_WEBVIEW_URL }}
       />
-    </View>
-    // <WebView
-    //   style={styles.container}
-    //   originWhitelist={["*"]}
-    //   source={{ uri: process.env.EXPO_PUBLIC_WEBVIEW_URL }}
-    // />
+    </>
   );
 }
 
