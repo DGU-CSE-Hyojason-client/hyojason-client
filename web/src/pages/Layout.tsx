27d599c22f9420e1d025395650c9dc6be809be12
jeonguk <leejeongwk98@gmail.com>
2024-06-06 00:08:25 +0900

    .

--- web/src/pages/Layout.tsx
+++ web/src/pages/Layout.tsx
@@ -1,14 +1,16 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import { GNB } from "../components/GNB";
 import Header from "../components/Header";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function Layout() {
+  const [msg, setMsg] = useState<string>("");
   const history = useNavigate();
 
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
-      const { route } = JSON.parse(event.data);
+      const { route } = event.data;
+      setMsg(event.data);
       history(route);
     };
 
@@ -22,6 +24,8 @@ export function Layout() {
   return (
     <div className="flex flex-col w-full h-screen bg-slate-900 text-white">
       <Header />
+      <div>{msg}</div>
+      <div onClick={() => history("/chat")}>to chat</div>
       <div className="flex grow overflow-hidden">
         <div className="h-full w-full overflow-auto">
           <Outlet />
