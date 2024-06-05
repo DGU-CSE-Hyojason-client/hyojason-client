9d5a71ebec02f53b297010d00f423cc86909a476
jeonguk <leejeongwk98@gmail.com>
2024-06-06 00:14:54 +0900

    .

--- native/App.js
+++ native/App.js
@@ -146,32 +146,6 @@ export default function App() {
   };
 
   return (
-    // <View
-    //   style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}
-    // >
-    //   <Text>Your expo push token: {expoPushToken}</Text>
-    //   <View style={{ alignItems: "center", justifyContent: "center" }}>
-    //     <WebView
-    //       style={styles.container}
-    //       originWhitelist={["*"]}
-    //       source={{ uri: process.env.EXPO_PUBLIC_WEBVIEW_URL }}
-    //     />
-    //     <Text>
-    //       Title: {notification && notification.request.content.title}{" "}
-    //     </Text>
-    //     <Text>Body: {notification && notification.request.content.body}</Text>
-    //     <Text>
-    //       Data:{" "}
-    //       {notification && JSON.stringify(notification.request.content.data)}
-    //     </Text>
-    //   </View>
-    //   <Button
-    //     title="Press to Send Notification"
-    //     onPress={async () => {
-    //       await sendPushNotification(expoPushToken);
-    //     }}
-    //   />
-    // </View>
     <>
       <View style={{ marginTop: __DEV__ ? 30 : 0 }}></View>
 
@@ -202,7 +176,7 @@ export default function App() {
         )}
         <View
           style={{
-            backgroundColor: "#1E293B",
+            backgroundColor: "#fef9f7",
             display: "flex",
             paddingRight: 64,
             paddingLeft: 64,
--- web/src/pages/Layout.tsx
+++ web/src/pages/Layout.tsx
@@ -1,16 +1,14 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import { GNB } from "../components/GNB";
 import Header from "../components/Header";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export function Layout() {
-  const [msg, setMsg] = useState<string>("");
   const history = useNavigate();
 
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
       const { route } = event.data;
-      setMsg(event.data);
       history(route);
     };
 
@@ -24,8 +22,6 @@ export function Layout() {
   return (
     <div className="flex flex-col w-full h-screen bg-slate-900 text-white">
       <Header />
-      <div>{msg}</div>
-      <div onClick={() => history("/chat")}>to chat</div>
       <div className="flex grow overflow-hidden">
         <div className="h-full w-full overflow-auto">
           <Outlet />
